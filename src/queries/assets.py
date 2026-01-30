69ae9b827f318af35f17f72357bbe0e5c6295bd8
Quentin Villotta <quentin.villotta@impact-initiatives.org>
2026-01-30 17:02:29 +0300

    Add country information to assets query
    Join with asset__settings__country to retrieve country name and code from settings, plus extract country code from project name

--- src/queries/assets.py
+++ src/queries/assets.py
@@ -25,23 +25,27 @@ def get_assets_data(
     
     query = """
     SELECT 
-        uid,
-        name,
-        owner__username,
-        owner__organization,
-        settings__organization,
-        settings__sector__label as sector,
-        date_created,
-        date_modified,
-        date_deployed,
-        deployment__last_submission_time as last_submission,
-        deployment__submission_count as submission_count,
-        deployment__active as is_active,
-        deployment_status,
-        has_deployment,
-        asset_type,
-        settings__description as description
-    FROM asset
+        a.uid,
+        a.name,
+        a.owner__username,
+        a.owner__organization,
+        a.settings__organization,
+        a.settings__sector__label as sector,
+        a.date_created,
+        a.date_modified,
+        a.date_deployed,
+        a.deployment__last_submission_time as last_submission,
+        a.deployment__submission_count as submission_count,
+        a.deployment__active as is_active,
+        a.deployment_status,
+        a.has_deployment,
+        a.asset_type,
+        a.settings__description as description,
+        cc.label as country_name,
+        cc.value as country_code_settings,
+        SUBSTRING(a.name FROM '^([A-Z]{3})') as country_code_from_project_name
+    FROM asset a
+    LEFT JOIN asset__settings__country cc ON a._dlt_id = cc._dlt_parent_id
     WHERE 1=1
     """
     
